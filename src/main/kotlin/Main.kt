0c189915640344689690bf3f894927182d4bf231
VicSchbt <victoire.schubert@gmail.com>
2025-02-19 14:27:45 +0100

    ✏️ compare user input and give feedbacks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,19 @@
 import java.util.Scanner
 
 fun main() {
+    println("Guess Number App\nTry to guess the number by enter a number.")
     val rand = (0..100).random()
+    var userNumber: Int? = null
 
-    // Read user input
-    println("Enter a number")
-    val line = readln()
-    println("You entered $line")
+    while (userNumber == null || userNumber != rand) {
+        println("Enter a number")
+        userNumber = readln().toInt()
+        if (userNumber > rand) {
+            println("Too high")
+        } else if (userNumber < rand) {
+            println("Too low")
+        }
+    }
+
+    println("Bravo! The answer was $rand.")
 }
