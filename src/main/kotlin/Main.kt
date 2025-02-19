690d75de09b383992449fddc084fbdcf38b3d09a
VicSchbt <victoire.schubert@gmail.com>
2025-02-19 15:20:52 +0100

    🧐check user inputs + improve feedback

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,13 @@
 fun main() {
     println("Guess Number App\nTry to guess the number by enter a number.")
     println("Enter the max range you want to play with:")
-    val range = readln().toInt()
+    val range = readUserInput()
 
     val user = User(range)
     val rand = (0..user.userRange).random()
 
     while (user.userNumber == null || user.userNumber != rand) {
-        println("Enter a number")
-        user.userNumber = readln().toInt()
+        user.userNumber = readUserInput()
         if (user.userNumber!! > rand) {
             println("Too high")
         } else if (user.userNumber!! < rand) {
@@ -17,5 +16,15 @@ fun main() {
         user.userAttempt++
     }
 
-    println("Bravo! The answer was $rand, and you try ${user.userAttempt} time(s).")
+    println("🎉 Bravo! The answer was $rand, and you try ${user.userAttempt} time(s).")
+}
+
+fun readUserInput(): Int {
+    var input: String? = null
+    while (input == null || input.toIntOrNull() == null) {
+        if(input != null)  println("You enter a wrong format 😬")
+        println("🎲 Enter a number")
+        input = readln()
+    }
+    return input.toInt()
 }
